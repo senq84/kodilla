0266577877cfc870a6af18d44f40a20e03f3d8ad
senq84 <93214097+senq84@users.noreply.github.com>
2021-11-15 16:57:38 +0100

    poprawka_1
    dołożona zmiana - tutaj printuje wyniki, jednak można zamienić na bool, bez printowania wyników - trzeba usunąć # z  line 7 line 10, a dodać # do line 8 i line 11 i będzie bool

--- modul4_2_z1.py
+++ modul4_2_z1.py
@@ -1,16 +1,22 @@
 
-def palindrom(word):
-  list(word)
-  list_of_letters = list(word)
-  def check_palindrom(list_of_letters):
-    if list_of_letters == list_of_letters[::-1]: 
-      print('True')
-    else:
-      print('False')
-  check_palindrom(list_of_letters)
+def palindrom(text):
+  alphanum = ''.join(ch for ch in text if ch.isalnum())
+  lower_letters = alphanum.lower()
+  list_of_letters = list(lower_letters)
+  if (list_of_letters == list_of_letters[::-1]):    
+#    return True 
+    print(f'true {text}')
+  else:
+#    return False
+    print('false')
 
+palindrom('Turb22 Burbon,erer')
+palindrom('Kajak')
 palindrom('kajak')
 palindrom('test')
-palindrom('kaziu')
+palindrom('kaziu uizak')
 palindrom('potop')
 palindrom('przypadkowe')
+palindrom('Turb022-_ Burbon,ererp')
+palindrom('alphanumeric@ 123__')
+palindrom('"Kobyła ma mały bok!"')
