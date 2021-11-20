6449e4584c5a96f0a274abae153fed1cf3c6f4bd
senq84 <93214097+senq84@users.noreply.github.com>
2021-11-20 17:58:08 +0100

    wersja 2 - odchudzona
    o niepotrzebne rzeczy. dalej działa

--- klasy_modul5_1.py
+++ klasy_modul5_1.py
@@ -1,12 +1,5 @@
-from typing import MutableMapping
-
 from faker import Faker
 fake = Faker()
-
-name = fake.name()
-company = fake.company()
-job = fake.job()
-email = fake.email()
 
 class businessCard:
     def __init__ (self, name, company, job, email):
@@ -14,8 +7,6 @@ class businessCard:
         self.company = company
         self.job = job
         self.email = email
-
-card = businessCard(fake.name(), fake.company(), fake.job(), fake.email())
 
 for _ in range(5):
     print(businessCard(fake.name(),fake.company(),fake.job(),fake.email()))
